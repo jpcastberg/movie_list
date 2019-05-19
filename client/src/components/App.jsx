7bc0baebfc998565733b7d41ebdc989bfc740a86
John Castberg <jpcastberg@gmail.com>
2019-05-18 18:01:04 -0700

    Set state on initial mount of App.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+import Heading from './Heading';
+import Components from './styled/Components';
+import movieData from '../exampleData';
 
-const App = () => (
-  <div>
-    Hello World
-  </div>
-);
+const { GlobalStyle } = Components;
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    setMovies(movies.concat(movieData));
+  }, []);
+  return (
+    <div>
+      <GlobalStyle />
+      <Heading />
+    </div>
+  );
+};
 
 export default App;
