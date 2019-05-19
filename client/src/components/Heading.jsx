f0d6b57323009c00a5f2c3a0c9a5f64d7c6ba2e5
John Castberg <jpcastberg@gmail.com>
2019-05-18 17:59:54 -0700

    Write heading

---
+++ client/src/components/Heading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Components from './styled/Components';
+
+const { H1, StyledHeading } = Components;
+
+const Heading = () => (
+  <StyledHeading>
+    <H1>Movie List</H1>
+  </StyledHeading>
+);
+
+export default Heading;
