6b7e33140a7244954332ba586cf931ff2ff580d7
John Castberg <jpcastberg@gmail.com>
2019-05-19 01:19:06 -0700

    Create Redux store

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,7 +6,11 @@ import rootReducer from './reducers';
 
 import App from './components/App';
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  // eslint-disable-next-line no-underscore-dangle
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 render(
   <Provider store={store}>
